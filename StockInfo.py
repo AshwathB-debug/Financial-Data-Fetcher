80b9ed432b4abf22ada996db8b10190d3eaa055c
AshwathB04 <balajia@vcu.edu>
2024-08-01 12:56:19 -0400

    StockInfo

--- StockInfo.py
+++ StockInfo.py
@@ -4,16 +4,24 @@ import os
 from dotenv import load_dotenv
 import webbrowser
 import requests
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
 
 
 # Upon calling this function, it will direct the user to the Nasdaq stock screener website with which they can look for a companies
 def openWebsite():
+    
     webbrowser.open_new("https://www.nasdaq.com/market-activity/stocks/screener")
     print("To find a companies stock ticker, refer to this website: https://www.nasdaq.com/market-activity/stocks/screener ")
 
 
 # This function is used to support the user if they are lost or need to refer to something to use the Stock information terminal. 
 def helpUser():
+    
     return """
           "The functions you can write are the following: ["OVERVIEW", "INCOME_STATEMENT", "BALANCE_SHEET", "CASH_FLOW", "EARNINGS"]
            The stock ticker can be any company you like but if you want more information on a stock, use the link to the Nasdaq stock screener provided.
@@ -27,12 +35,14 @@ def helpUser():
     
 # This function has an array of the different functions the user can choose from to get information about a stock. 
 def listOfFunctions(function):
+    
     arrOfFunctions = ["OVERVIEW", "INCOME_STATEMENT", "BALANCE_SHEET", "CASH_FLOW", "EARNINGS"]
     return function in arrOfFunctions
 
 
 # This function is used to pass in the ticker symbol and type of investment to find the stock ticker and return it.
 def stockInfo(APIkey, ticker, typeOfInvestment):
+    
     link = f"https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={ticker}&apikey={APIkey}&datatype=csv"
     df = pd.read_csv(link)
     
@@ -44,28 +54,68 @@ def stockInfo(APIkey, ticker, typeOfInvestment):
             return "The ticker, country, or type of investment you entered does not exist in our database. Please check for incorrect spelling or spaces. Refer to the help command for any doubts."
         
     except:
-        return "You have use the stockInfo function too many times. Please try again later. "
+        return "You have used the stockInfo function too many times. Please try again later. "
 
 
 # This function reads the link using Python's requests module and writes the data from the link into a csv file and returns it
+
+stock_data = "stock_data.csv"
+
 def companyInfo(FUNCTION, ticker, APIkey):
+    
     link = f"https://www.alphavantage.co/query?function={FUNCTION}&symbol={ticker}&apikey={APIkey}"
     
     try:
 
         r = requests.get(link)
         r.raise_for_status()
-        stock_data = "stock_data.csv"
         
         with open(stock_data, "wb") as file:
             file.write(r.content)
+        sendEmail(stock_data)
         return f"The CSV file for the ticker you entered has been created successfully: {stock_data}"
         
     except Exception as e:
         return f"An unexpected error occurred: {e}"
     
 
-# The main function executes the functions and accesses the API key to get all the information from Alpha Vantage's website   
+def sendEmail(stock_data):
+    
+    load_dotenv()
+    password = os.getenv("app_password")
+    
+    sender = input("SENDER EMAIL: ")
+    receiver = input("RECEIVER EMAIL: ")
+    subject = input("SUBJECT: ")
+    
+    
+    message = MIMEMultipart()
+    message["From"] = sender
+    message["To"] = receiver
+    message["Subject"] = subject
+    
+    file = open(stock_data, "rb")
+    
+    
+    # To encode it in base 64
+    file_package = MIMEBase("application", "octet-stream")
+    file_package.set_payload((file).read())
+    encoders.encode_base64(file_package)
+    file_package.add_header("Content-Disposition", "attachment; filename= " + stock_data)
+    message.attach(file_package)
+    
+    text = message.as_string()
+    
+    s = smtplib.SMTP("smtp.gmail.com", 587)
+    s.starttls()
+    s.login(sender, password)
+    s.sendmail(sender, receiver, text)
+        
+    return f"Email has been sent to {receiver}! "
+
+
+# The main function executes the functions and accesses the API key to get all the information from Alpha Vantage's website 
+  
 if __name__ == "__main__":
     load_dotenv()
     APIkey = os.getenv("API_key")
@@ -88,7 +138,7 @@ if __name__ == "__main__":
                 typeOfInvestment = input("Enter the type of investment: ").strip().title()
                 
                 print(stockInfo(APIkey, ticker, typeOfInvestment))
-                print(companyInfo(nameOfFunction, ticker, APIkey))      
+                print(companyInfo(nameOfFunction, ticker, APIkey))   
                     
             else:
                 print("The function you typed is incorrect. Please refer to the help function. ")
